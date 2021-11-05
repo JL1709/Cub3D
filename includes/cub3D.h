d2d96b0794c6164542504bcda98222d30b1d3c53
Julian Ludt <jludt@2-C-2.42heilbronn.de>
2021-11-05 13:53:41 +0100

    framework

---
+++ includes/cub3D.h
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3D.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jludt <jludt@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/10 15:08:57 by julian            #+#    #+#             */
+/*   Updated: 2021/11/05 13:41:35 by jludt            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include <stdio.h>
+# include <math.h>
+# include <errno.h>
+# include "../mlx/mlx.h"
+# include "../libft/libft.h"
+
+# define WINDOW_WIDTH	1280
+# define WINDOW_HEIGHT	720
+
+// typedef struct s_data
+// {
+// 	void	*init;
+// 	void	*win;
+// 	void	*img;
+// 	char	*data_addr;
+// 	int		bits_per_pixel;
+// 	int		size_line;
+// 	int		endian;
+// }			t_data;
+
+#endif
